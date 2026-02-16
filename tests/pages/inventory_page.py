fefc39cbb9ac0143a1c564a99ed61f4887550931
loaihashim1999 <loai.h.k99@gmail.com>
2026-02-16 16:44:39 +0300

    Fix: Improve cart count wait condition in inventory page
    - Add explicit wait for add-to-cart button to be clickable
    - Wait for cart badge to display text '1' after adding product
    - Use text_to_be_present_in_element to ensure cart updates
    - Prevents race condition where cart badge doesn't update in time
    - Fixes failing test_add_product_to_cart assertion

--- tests/pages/inventory_page.py
+++ tests/pages/inventory_page.py
@@ -6,25 +6,33 @@ class InventoryPage:
     '''
     Page Object Model لصفحة المنتجات
     '''
-    
-    # Locators (محدثة)
+
+    # Locators
     PRODUCTS_TITLE = (By.CLASS_NAME, 'title')
     ADD_TO_CART_BUTTON = (By.ID, 'add-to-cart-sauce-labs-backpack')
     CART_BADGE = (By.CSS_SELECTOR, 'span.shopping_cart_badge')
     CART_BUTTON = (By.CLASS_NAME, 'shopping_cart_link')
-    
+
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
-    
+
     def get_title(self):
         '''الحصول على عنوان الصفحة'''
         return self.driver.find_element(*self.PRODUCTS_TITLE).text
-    
+
     def add_product_to_cart(self):
         '''إضافة منتج للسلة'''
-        self.driver.find_element(*self.ADD_TO_CART_BUTTON).click()
-    
+        add_button = self.wait.until(
+            EC.element_to_be_clickable(self.ADD_TO_CART_BUTTON)
+        )
+        add_button.click()
+        
+        # انتظر حتى يظهر شارة السلة برقم 1
+        self.wait.until(
+            EC.text_to_be_present_in_element(self.CART_BADGE, '1')
+        )
+
     def get_cart_count(self):
         '''الحصول على عدد المنتجات في السلة'''
         try:
@@ -34,7 +42,7 @@ class InventoryPage:
             return element.text
         except:
             return '0'
-    
+
     def click_cart(self):
         '''النقر على زر السلة'''
         self.driver.find_element(*self.CART_BUTTON).click()
