e4f3b8bd4353f1fabd2077e7469afdbe3e3c335a
loaihashim1999 <loai.h.k99@gmail.com>
2026-02-16 17:09:52 +0300

    Fix: Use explicit wait for cart badge in inventory test

--- tests/pages/inventory_page.py
+++ tests/pages/inventory_page.py
@@ -3,9 +3,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class InventoryPage:
+    '''
+    Page Object Model ????? ????????
+    '''
+    
+    # Locators
     PRODUCTS_TITLE = (By.CLASS_NAME, 'title')
     ADD_TO_CART_BUTTON = (By.ID, 'add-to-cart-sauce-labs-backpack')
-    CART_BADGE = (By.CSS_SELECTOR, 'span.shopping_cart_badge')
+    CART_BADGE = (By.CSS_SELECTOR, 'div.shopping_cart_badge')
     CART_BUTTON = (By.CLASS_NAME, 'shopping_cart_link')
     
     def __init__(self, driver):
@@ -13,17 +18,24 @@ class InventoryPage:
         self.wait = WebDriverWait(driver, 10)
     
     def get_title(self):
+        '''?????? ??? ????? ??????'''
         return self.driver.find_element(*self.PRODUCTS_TITLE).text
     
     def add_product_to_cart(self):
+        '''????? ???? ?????'''
         self.driver.find_element(*self.ADD_TO_CART_BUTTON).click()
     
     def get_cart_count(self):
+        '''?????? ??? ??? ???????? ?? ????? ?? ??????'''
         try:
-            element = self.wait.until(EC.visibility_of_element_located(self.CART_BADGE))
+            # ?????? ???? ?????? ????????
+            element = self.wait.until(
+                EC.visibility_of_element_located(self.CART_BADGE)
+            )
             return element.text
         except:
             return '0'
     
     def click_cart(self):
+        '''????? ??? ?? ?????'''
         self.driver.find_element(*self.CART_BUTTON).click()
